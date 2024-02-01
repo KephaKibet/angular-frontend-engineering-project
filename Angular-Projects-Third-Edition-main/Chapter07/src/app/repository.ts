332b7cc2710be7b060d92c0e84dde069d9b6d276
Kepha Kibet <77564875+KephaKibet@users.noreply.github.com>
2024-02-01 15:37:26 +0300

    Delete Angular-Projects-Third-Edition-main directory

--- Angular-Projects-Third-Edition-main/Chapter07/src/app/repository.ts
+++
@@ -1,9 +0,0 @@
-export interface Repository {
-  name: string;
-  html_url: string;
-  description: string;
-  fork: boolean;
-  stargazers_count: number;
-  language: string;
-  forks_count: number;
-}
--- Angular-Projects-Third-Edition-main/Chapter07/src/app/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  avatar_url: string;
-  name: string;
-  blog: string;
-  location: string;
-  bio: string;
-  twitter_username: string;
-  followers: number;
-}
